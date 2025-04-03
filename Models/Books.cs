673b1d81e0dec0bcd3812a062058dfdf54e475b2
tderamus <deramust@gmail.com>
2025-04-02 23:56:04 -0500

    fixed db context file to correct type errors. Connected backend application to the database

--- Models/Books.cs
+++ Models/Books.cs
@@ -5,13 +5,14 @@ namespace SimplyBooksBE.Models
 {
     public class Books
     {
+        [Key]
         required public string uid { get; set; }
-        public string AuthorId { get; set; } // Foreign key to Authors
-        public Authors Author { get; set; }
-        public string firebaseKey { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public string image { get; set; }
+        public string? AuthorId { get; set; } // Foreign key to Authors
+        public Authors? Author { get; set; }
+        public string? firebaseKey { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public string? image { get; set; }
         public decimal price { get; set; } = 0.00m;
         public bool sale { get; set; } = false;
     }
--- SimplyBooksDbContext.cs
+++ SimplyBooksDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
 using SimplyBooksBE.Models;
 
     public class SimplyBooksDbContext : DbContext
@@ -14,7 +13,7 @@ using SimplyBooksBE.Models;
     {
         // Set the relationship between Authors and Books
         modelBuilder.Entity<Books>()
-            .HasOne(b => b.AuthorId)
+            .HasOne(b => b.Author)
             .WithMany()
             .HasForeignKey(b => b.AuthorId) // This is the foreign key in the Books table
             .IsRequired(false);
