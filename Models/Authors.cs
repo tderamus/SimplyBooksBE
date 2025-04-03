6c229c065ab8aed96b88a21b17247b8ff979d5d6
tderamus <deramust@gmail.com>
2025-04-02 23:14:28 -0500

    added db context file and database clases for initial migration '

---
+++ Models/Authors.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimplyBooksBE.Models
+{
+    public class Authors
+    {
+        required public string uid { get; set; }
+        public string firebaseKey { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string image { get; set; }
+        public bool favorite { get; set; } = false;
+    }
+}
---
+++ SimplyBooksDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using SimplyBooksBE.Models;
+
+    public class SimplyBooksDbContext : DbContext
+{
+    public SimplyBooksDbContext(DbContextOptions<SimplyBooksDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Authors> Authors { get; set; }
+    public DbSet<Books> Books { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Set the relationship between Authors and Books
+        modelBuilder.Entity<Books>()
+            .HasOne(b => b.AuthorId)
+            .WithMany()
+            .HasForeignKey("AuthorId") // This is the foreign key in the Books table
+            .IsRequired(false); // Allow nulls if needed, otherwise set to true
+
+    }
+}
